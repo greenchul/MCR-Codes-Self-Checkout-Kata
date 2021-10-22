36e1327a08229aefd163d90fec486d9d531dad48
Rachel Sanham <green_chul@hotmail.com>
2021-10-22 08:17:32 +0100

    refactored to class based

--- __tests__/self-checkout.test.js
+++ __tests__/self-checkout.test.js
@@ -4,37 +4,38 @@
 // 3 - As a user, I would like to be able to see the total price of all items in my basket.
 // 4 - As a user, I would like to be able to remove an item from my basket
 
-const { scanItem, customerBasket, addItemToBasket, calculateTotal, removeItem } = require("../index")
-const { orange, apple, banana, pineapple, kiwi, items } = require("../data")
+const { SelfCheckout } = require("../index");
+const { orange, apple, banana, pineapple, kiwi, items } = require("../data");
 
-describe("scanItem", ()=>{
-    it("should return product details as object when passed barcode", ()=>{
-        expect(scanItem(kiwi.barcode)).toBe(kiwi)
-    })
-})
+const mySelfCheckout = new SelfCheckout();
 
-describe("add item to basket",()=>{
-    it("should add an item to the customers basket", ()=>{
-        expect(customerBasket).toEqual([])
-        addItemToBasket(orange)
-        expect(customerBasket.length).toEqual(1)
-    })
-})
+describe("scanItem", () => {
+  it("should return product details as object when passed barcode", () => {
+    expect(mySelfCheckout.scanItem(kiwi.barcode)).toBe(kiwi);
+  });
+});
 
-describe("Calculate the total price of the basket",()=>{
-    it("Should equal 7 when basket contains 1 orange and nothing else", ()=>{
-        expect(calculateTotal(customerBasket)).toBe(7)
-        
-    })
-    it("Should equal 32 when basket contains 1 orange and 1 kiwi and nothing else",()=>{
-        addItemToBasket(kiwi)
-        expect(calculateTotal(customerBasket)).toBe(32)
-    })
-})
+describe("add item to basket", () => {
+  it("should add an item to the customers basket", () => {
+    expect(mySelfCheckout.basket).toEqual([]);
+    mySelfCheckout.addItemToBasket(orange);
+    expect(mySelfCheckout.basket.length).toEqual(1);
+  });
+});
 
-describe("Remove specified item from basket",()=>{
-    it("Should remove kiwi from basket and contain only orange",()=>{
-        removeItem(kiwi)
-        expect(customerBasket.length).toEqual(1)
-    })
-} )
+describe("Calculate the total price of the basket", () => {
+  it("Should equal 7 when basket contains 1 orange and nothing else", () => {
+    expect(mySelfCheckout.calculateTotal()).toBe(7);
+  });
+  it("Should equal 32 when basket contains 1 orange and 1 kiwi and nothing else", () => {
+    mySelfCheckout.addItemToBasket(kiwi);
+    expect(mySelfCheckout.calculateTotal()).toBe(32);
+  });
+});
+
+describe("Remove specified item from basket", () => {
+  it("Should remove kiwi from basket and contain only orange", () => {
+    mySelfCheckout.removeItem(kiwi);
+    expect(mySelfCheckout.basket.length).toEqual(1);
+  });
+});
--- index.js
+++ index.js
@@ -1,43 +1,35 @@
-
-
 // 3 - As a user, I would like to be able to see the total price of all items in my basket.
 // 4 - As a user, I would like to be able to remove an item from my basket
 
-const { orange, apple, banana, pineapple, kiwi, items } = require("./data")
+const { orange, apple, banana, pineapple, kiwi, items } = require("./data");
 
-let customerBasket = []
-
-
-// 1- As a user, I would like to be able to scan an item using barcode so its details can be found from products list (test data).
-
-const scanItem = (barcode)=>{
-    let result = items.find((product)=>{
-        return product.barcode === barcode
-    })
-    return result
+class SelfCheckout {
+  constructor() {
+    this.basket = [];
+  }
+  scanItem(barcode) {
+    const result = items.find((product) => {
+      return product.barcode === barcode;
+    });
+    return result;
+  }
+  addItemToBasket(item) {
+    this.basket.push(item);
+  }
+  calculateTotal() {
+    const total = this.basket.reduce((acc, currentItem) => {
+      return acc + currentItem.price;
+    }, 0);
+    return total;
+  }
+  removeItem(item) {
+    const itemIndex = this.basket.findIndex((item) => {
+      return item;
+    });
+    this.basket.splice(itemIndex, 1);
+  }
 }
 
-
-// 2 - As a user, I would like to be able to add an item to basket.
-const addItemToBasket = (item) =>{
-    customerBasket.push(item)
-}
-
-
-// 3 - As a user, I would like to be able to see the total price of all items in my basket.
-const calculateTotal = (basket) =>{
-   let total =  basket.reduce((acc, currentItem)=>{
-        return acc + currentItem.price
-    },0)
-    return total
-}
-
-const removeItem = (item) =>{
-    let itemIndex = customerBasket.findIndex((item)=>{
-        return item
-    })
-    customerBasket.splice(itemIndex, 1)
-}
-
-
-module.exports = { scanItem, customerBasket, addItemToBasket, calculateTotal, removeItem}
+module.exports = {
+  SelfCheckout,
+};
